6f2d8772edbeba0c1f9f405fd7c418104c2d266a
KimDongWook <kimdw3824@gmail.com>
2024-05-11 11:34:23 +0900

    바텀네비게이션바 완성

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:simple_social_app/src/pages/friends_list_page.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int pageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: IndexedStack(
+        index: pageIndex,
+        children: [
+          const FriendsListPage(),
+          Container(color: const Color.fromARGB(255, 111, 172, 222)),
+          Container(color: const Color.fromARGB(255, 129, 236, 132)),
+          Container(color: const Color.fromARGB(255, 238, 228, 135)),
+          Container(color: const Color.fromARGB(255, 84, 81, 81)),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: pageIndex,
+        onTap: (value) {
+          setState(() {
+            pageIndex = value;
+          });
+        },
+        selectedItemColor: Colors.blue[400],
+        unselectedItemColor: Colors.grey,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: ""),
-          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: ""),
-          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: ""),
-          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: ""),
-          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: ""),
         ],
       ),
     );
