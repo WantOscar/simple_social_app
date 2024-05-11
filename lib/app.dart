7b2a705c17198f7857fa04140e186c8664fac458
KimDongWook <kimdw3824@gmail.com>
2024-05-11 12:10:00 +0900

    탐색 창과 친구 목록 리스트 제작 완료

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:simple_social_app/src/pages/friends_list_page.dart';
 
 class App extends StatefulWidget {
--- lib/src/pages/friends_list_page.dart
+++ lib/src/pages/friends_list_page.dart
@@ -5,6 +5,54 @@ class FriendsListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back_ios, color: Colors.black87),
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: [
+          _searchBar(),
+          _friendsList(),
+        ],
+      ),
+    );
   }
+
+  Widget _searchBar() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: Container(
+          height: 50,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10), color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _friendsList() => Expanded(
+        child: Center(
+          child: Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+              color: Colors.white,
+            ),
+            child: ListView.builder(
+              itemCount: 30,
+              itemBuilder: (context, index) => const ListTile(
+                title: Text('익명'),
+                subtitle: Text('010-6546-9879'),
+              ),
+            ),
+          ),
+        ),
+      );
 }
